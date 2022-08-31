209604b2ef5b4ac403c86341cb685d1cabbbf738
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-08-31 12:20:09 -0400

    Create ComplementaryDna class

---
+++ src/main/java/kata/solutions/ComplementaryDna.java
@@ -0,0 +1,26 @@
+package kata.solutions;
+
+/**
+ * Abbreviate A Two Word Name
+ *
+ * Write a function to convert a name into initials.
+ * This kata strictly takes two words with one space in between them.
+ * The output should be two capital letters with a dot separating them.
+ *
+ * EXAMPLE 1:
+ * input: Sam Harris
+ * output: S.H
+ *
+ * EXAMPLE 2:
+ * input: patrick feeney
+ * output: P.F
+ *
+ * EXAMPLE 3:
+ * input: P Favuzzi
+ * output: P.F
+ *
+ * @see <a href="https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/java">Abbreviate A Two Word Name</a>
+ */
+
+public class ComplementaryDna {
+}
