fc8226ef933da031369cc621e1aae4abdbdc3898
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-08-31 13:05:45 -0400

    Update ComplementaryDna.java
    updated comments to reflect intent

--- src/main/java/kata/solutions/ComplementaryDna.java
+++ src/main/java/kata/solutions/ComplementaryDna.java
@@ -20,8 +20,31 @@ package kata.solutions;
  */
 
 public class ComplementaryDna {
-    public static String makeCOmplement(String dna){
+    public static String makeComplement(String dna){
+        //convert string to char array
+        char[] ch = dna.toCharArray();
 
-        return dna;
+        //loop through array of characters
+        for(int i = 0; i < ch.length; i++){
+            //set character equal to it's complement
+            ch[i] = getComplement(ch[i]);
+        }
+
+        //return the new value of DNA
+        return String.valueOf(ch);
+    }
+
+    private static char getComplement(char c){
+        //
+        switch(c){
+            //intellij wanted the return statement wrapped???
+            //not very INTELLIGENT ;D amirite?
+            case 'A' -> {return 'T';}
+            case 'T' -> {return 'A';}
+            case 'G' -> {return 'C';}
+            case 'C' -> {return 'G';}
+        }
+        //return the converted character
+        return c;
     }
 }
